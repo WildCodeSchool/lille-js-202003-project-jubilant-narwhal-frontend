2f71c50b83c8ec453354c20f4e299ff1bead5599
sophie <sneuville59@gmail.com>
2020-06-10 16:39:54 +0200

    add TextInput

---
+++ src/components/Text/TextInput.tsx
@@ -0,0 +1,8 @@
+import styled from 'rn-css'
+
+const TextInput = styled.Text`
+  color: black:
+  font-size:2em;
+  border: 2px solid purple;
+`
+export default TextInput
---
+++ src/components/Text/TextMatiere.tsx
@@ -0,0 +1,6 @@
+import styled from 'rn-css'
+const TextMatiere = styled.Text`
+  color: yellow:
+  font-size:3em;
+`
+export default TextMatiere
