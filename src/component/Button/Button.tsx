cbee17ad1b830d3aa118ebb1b1aeefbd698e069f
magb7 <margaux.bedu@gmail.com>
2020-06-10 17:34:17 +0200

    create stories folder and add test story for button

---
+++ src/component/Button/Button.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button, View } from "react-native";
+
+const CustomButton = () => {
+  return (
+    <View>
+      <Button title="test" />
+    </View>
+  );
+};
+
+export default CustomButton;
---
+++ stories/Button/Button.stories.js
@@ -0,0 +1,8 @@
+import React from "react";
+import CustomButton from "../../src/component/Button/Button";
+
+export default {
+  title: "Button",
+};
+
+export const button = () => <CustomButton />;
