4521ac1ab1c4897ae1b85960b6514d05d394e6b9
magb7 <margaux.bedu@gmail.com>
2020-06-11 16:12:15 +0200

    create stories folder

---
+++ stories/Components.stories.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import FlecheDroiteBtn from '../src/components/Button/FlecheDroiteBtn'
+
+export default {
+  title: 'Components'
+}
+
+export const FlecheDroiteStory = () => <FlecheDroiteBtn />
