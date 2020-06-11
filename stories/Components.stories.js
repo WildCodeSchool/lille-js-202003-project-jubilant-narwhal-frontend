cebcc1a699c69722aeee0de1492403c55fb03591
magb7 <margaux.bedu@gmail.com>
2020-06-11 17:20:29 +0200

    add stories for two components

--- stories/Components.stories.js
+++ stories/Components.stories.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import FlecheDroiteBtn from '../src/components/Button/FlecheDroiteBtn'
+import DayOffOverlay from '../src/components/DayOffOverlay'
+import TaskDoneOverlay from '../src/components/TaskDoneOverlay'
 
 export default {
   title: 'Components'
 }
 
 export const FlecheDroiteStory = () => <FlecheDroiteBtn />
+
+export const DayOffOverlayStory = () => <DayOffOverlay />
+
+export const TaskDoneOverlayStory = () => <TaskDoneOverlay />
