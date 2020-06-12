99d6713b53ecd5bbd3a985d96e19ac2c9b3b1b71
magb7 <margaux.bedu@gmail.com>
2020-06-12 17:43:21 +0200

    create containers folder

---
+++ src/containers/SwitchView.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import styled from 'rn-css'
+import ViewLeftBtn from '../components/Button/ViewLeftBtn'
+import ViewRightBtn from '../components/Button/ViewRightBtn'
+
+const Container = styled.View`
+    width: 300px;
+    flex-direction: row;
+    justify-content: center;
+    padding: 0;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+`
+const SwitchView = () => {
+  return (
+    <Container>
+      <ViewLeftBtn />
+      <ViewRightBtn />
+    </Container>
+  )
+}
+
+export default SwitchView
