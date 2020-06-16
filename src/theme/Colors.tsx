f4548f0489a55ac84b2e8b47174d9682c60df5aa
magb7 <margaux.bedu@gmail.com>
2020-06-16 16:37:53 +0200

    create theme folder

---
+++ src/theme/Colors.tsx
@@ -0,0 +1,10 @@
+export const Colors = {
+  cardBckg: {
+    francais: 'rgba(141, 71, 214, 0.7)',
+    maths: 'rgba(88, 199, 234, 0.7)',
+    svt: 'rgba(105, 209, 130, 0.7)',
+    physChim: 'rgba(242, 162, 51, 0.7)',
+    langues: 'rgba(44, 116, 251, 0.7)',
+    histoire: 'rgba(228, 65, 99, 0.7)'
+  }
+}
