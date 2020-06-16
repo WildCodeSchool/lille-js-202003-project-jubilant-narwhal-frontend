42587021f3ce48c2d9014a726cc27147f5feb94f
magb7 <margaux.bedu@gmail.com>
2020-06-16 11:34:23 +0200

    import new component

--- stories/Components.stories.js
+++ stories/Components.stories.js
@@ -5,18 +5,17 @@ import MatiereBtn from '../src/components/Button/MatiereBtn'
 import SendBtn from '../src/components/Button/SendBtn'
 import PenBtn from '../src/components/Button/PenBtn'
 import ValidateBtn from '../src/components/Button/ValidateBtn'
-import App from '../src/index'
 import DayOffOverlay from '../src/components/DayOffOverlay'
 import TaskDoneOverlay from '../src/components/TaskDoneOverlay'
 import ViewLeftBtn from '../src/components/Button/ViewLeftBtn'
 import ViewRightBtn from '../src/components/Button/ViewRightBtn'
 import SwitchViewBtn from '../src/components/Button/SwitchViewBtn'
+import DayColumn from '../src/components/DayColumn'
 
 export default {
   title: 'Components'
 }
 
-export const IndexStory = () => <App />
 export const ValidateBtnStory = () => <ValidateBtn />
 export const SendBtnStory = () => <SendBtn />
 export const MatiereBtnStory = () => <MatiereBtn />
@@ -28,3 +27,4 @@ export const ViewLeftBtnStory = () => <ViewLeftBtn />
 export const ViewRightBtnStory = () => <ViewRightBtn />
 export const PenBtnStory = () => <PenBtn />
 export const SwitchViewBtnStory = () => <SwitchViewBtn />
+export const DayColumnStory = () => <DayColumn/>
