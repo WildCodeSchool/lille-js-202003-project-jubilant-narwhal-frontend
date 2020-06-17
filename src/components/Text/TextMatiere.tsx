956b7183b94b24fc741e793253969e77a2c74283
magb7 <margaux.bedu@gmail.com>
2020-06-17 10:25:30 +0200

    delete useless component

--- src/components/Text/TextMatiere.tsx
+++
@@ -1,7 +0,0 @@
-import styled from 'rn-css'
-
-const TextMatiere = styled.Text`
-  color: yellow;
-  font-size: 3em;
-`
-export default TextMatiere
--- src/components/Text/TextValidate.tsx
+++
@@ -1,6 +0,0 @@
-import styled from 'rn-css'
-const TextValidate = styled.Text`
-  color: orange:
-  font-size:3em;
-`
-export default TextValidate
