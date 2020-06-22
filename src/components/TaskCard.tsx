e83e3a57ced9e65b519a59da980ff8d4b7eaf503
magb7 <margaux.bedu@gmail.com>
2020-06-22 10:46:12 +0200

    fix color import

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -1,48 +1,49 @@
-// import React from 'react'
-// import styled from 'rn-css'
-// import ColorsSubjects from '../../theme/Colors'
-// import PenBtn from './Button/PenBtn'
-// import Checkbox from './Button/CheckBox'
+import React from 'react'
+import styled from 'rn-css'
+import ColorsSubjects from '../theme/Colors'
+import PenBtn from './Button/PenBtn'
+import Checkbox from './Button/CheckBox'
 
-// const TaskCardWrapper = styled.View<{ color: string }>`
-//   padding: 20px;
-//   background: ${props => props.color};
-//   border-radius: 10px;
-//   justify-content: space-between;
-//   width: 459px;
-//   height: 119px;
-// `
-// const ViewCardRow = styled.View`
-//   flex-direction: row;
-//   justify-content: space-between;
-//   align-items: center;
-// `
-// const Placeholder = styled.View`
-//   margin-right: 10px;
-//   width: 30px;
-//   height: 30px;
-//   background: gray;
-// `
-// const TaskCardText = styled.Text`
-//   color: white;
-//   font-size: 2em;
-// `
-// const TaskCard = () => {
-//   return (
-//     <TaskCardWrapper color={Colors.cardBckg.svt}>
-//       <ViewCardRow>
-//         <ViewCardRow>
-//           <Placeholder />
-//           <TaskCardText>Matière</TaskCardText>
-//         </ViewCardRow>
-//         <PenBtn />
-//       </ViewCardRow>
-//       <ViewCardRow>
-//         <TaskCardText>Devoir à faire</TaskCardText>
-//         <Checkbox />
-//       </ViewCardRow>
-//     </TaskCardWrapper>
-//   )
-// }
+const TaskCardWrapper = styled.View<{ color: string }>`
+  padding: 20px;
+  background: ${props => props.color};
+  border-radius: 10px;
+  justify-content: space-between;
+  width: 459px;
+  height: 119px;
+`
+const ViewCardRow = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`
+const Placeholder = styled.View`
+  margin-right: 10px;
+  width: 30px;
+  height: 30px;
+  background: gray;
+`
 
-// export default TaskCard
+const TaskCardText = styled.Text`
+  color: white;
+  font-size: 2em;
+`
+const TaskCard = () => {
+  return (
+    <TaskCardWrapper color={ColorsSubjects[0].color}>
+      <ViewCardRow>
+        <ViewCardRow>
+          <Placeholder />
+          <TaskCardText>Matière</TaskCardText>
+        </ViewCardRow>
+        <PenBtn />
+      </ViewCardRow>
+      <ViewCardRow>
+        <TaskCardText>Devoir à faire</TaskCardText>
+        <Checkbox />
+      </ViewCardRow>
+    </TaskCardWrapper>
+  )
+}
+
+export default TaskCard
