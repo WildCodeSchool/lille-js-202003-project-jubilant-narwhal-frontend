59449cc3dbee80657ef5f1739d1f85589fe7f986
magb7 <margaux.bedu@gmail.com>
2020-06-22 10:43:17 +0200

    fix color

--- src/components/Button/ValidateBtn.tsx
+++ src/components/Button/ValidateBtn.tsx
@@ -3,12 +3,12 @@ import React from 'react'
 import { Text } from 'react-native'
 
 const ValidateBtnStyled = styled.TouchableOpacity`
-  background: rgba(154, 205, 50, 0, 7);
+  background: rgba(154, 205, 50, 0.7);
   justify-content: center;
   align-items: center;
   width: 100px;
   height: 50px;
-  border: 2px solid rgba(0, 0, 0, 1);
+  border: 2px solid 'rgba(0,0,0,1)';
   pressduration: 0.3;
 `
 
