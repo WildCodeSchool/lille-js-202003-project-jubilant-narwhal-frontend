9288df19474fb65d1fc37ef69f672f1dc523b49e
magb7 <margaux.bedu@gmail.com>
2020-06-22 18:18:36 +0200

    add new key

--- src/theme/Colors.tsx
+++ src/theme/Colors.tsx
@@ -1,10 +1,26 @@
 const ColorsSubjects = [
-  { subject: 'Français', color: 'rgba(141, 71, 214, 0.7)' },
-  { subject: 'Mathématiques', color: 'rgba(88, 199, 234, 0.7)' },
-  { subject: 'Langues vivantes', color: 'rgba(44, 116, 251, 0.7)' },
-  { subject: 'SVT', color: 'rgba(105, 209, 130, 0.7)' },
-  { subject: 'Physique_chimie', color: 'rgba(242, 162, 51, 0.7)' },
-  { subject: 'Histoire_géographie', color: 'rgba(228, 65, 99, 0.7)' }
+  { label: 'Français', color: 'rgba(141, 71, 214, 0.7)', selected: false },
+  {
+    label: 'Mathématiques',
+    color: 'rgba(88, 199, 234, 0.7)',
+    selected: false
+  },
+  {
+    label: 'Langues vivantes',
+    color: 'rgba(44, 116, 251, 0.7)',
+    selected: false
+  },
+  { label: 'SVT', color: 'rgba(105, 209, 130, 0.7)', selected: false },
+  {
+    label: 'Physique_chimie',
+    color: 'rgba(242, 162, 51, 0.7)',
+    selected: false
+  },
+  {
+    label: 'Histoire_géographie',
+    color: 'rgba(228, 65, 99, 0.7)',
+    selected: false
+  }
 ]
 
 export default ColorsSubjects
