b0c691063a5f0a05b97fbd05fdcd69b4d3c537ad
magb7 <margaux.bedu@gmail.com>
2020-06-23 11:52:13 +0200

    create new folder for screens and a AddTaskScreen file

---
+++ stories/Screens.stories.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import AddTaskScreen from '../src/screens/AddTaskScreen'
+
+export default {
+  title: 'Screens'
+}
+
+export const AddTaskScreenStory = () => <AddTaskScreen />
