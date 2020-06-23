b562c029687b20596b6a19e3a482b108772c7bdb
magb7 <margaux.bedu@gmail.com>
2020-06-23 12:32:00 +0200

    create a functional component

--- src/containers/AddTaskContainer.tsx
+++ src/containers/AddTaskContainer.tsx
@@ -4,17 +4,15 @@ import styled from 'rn-css'
 
 import { SubjectsColors, ProjectColors } from '../theme/Colors'
 import MatiereBtn from '../components/Button/MatiereBtn'
+import CrossBtn from '../components/Button/CrossBtn'
+import TextInput from '../components/Text/TextInput'
+import ValidateBtn from '../components/Button/ValidateBtn'
 
 // Styled Components
-const ModalContainer = styled.View`
-  width: 100vw;
-  height: 100vh;
-  background-color: rgba(0, 0, 0, 0.5);
-  justify-content: center;
-  align-items: center;
-`
 const AddTaskWrapper = styled.View<{ color: any }>`
+  padding: 20px;
   align-items: center;
+  justify-content: space-around;
   background-color: rgba(255, 255, 255, 1);
   width: 75vw;
   height: 70vh;
@@ -22,16 +20,27 @@ const AddTaskWrapper = styled.View<{ color: any }>`
   border-width: 2px;
   border-color: ${props => props.color.primary};
 `
+
+const TitleWrapper = styled.View`
+  width: 100%;
+  border: 1px solid yellowgreen;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: flex-start;
+`
+
 const ModalTitle = styled.Text<{ color: any }>`
+  justify-self: center;
+  align-self: center;
   color: ${props => props.color.primary};
-  font-size: 3em;
+  font-size: 2em;
 `
 
 const BtnWrapper = styled.View`
+  width: 100%;
   flex-direction: row;
   border: 1px solid red;
   justify-content: space-around;
-  width: 50vw;
 `
 // Variables for the state
 const InitialState = [...SubjectsColors]
@@ -57,22 +66,29 @@ const AddTaskContainer = () => {
     setSelectedSub(subjectTheme[nb].label)
   }
 
+  const closeWindow = () => {
+    return console.log('close window')
+  }
+
   return (
-    <ModalContainer>
-      <AddTaskWrapper color={ProjectColors}>
-        <ModalTitle color={ProjectColors}>Ajoute un devoir à faire</ModalTitle>
+    <AddTaskWrapper color={ProjectColors}>
+      <TitleWrapper>
         <Text>{selectedSubject}</Text>
-        <BtnWrapper>
-          {subjectTheme.map((sub, index) => (
-            <MatiereBtn
-              subject={sub}
-              handleChange={() => handleChange(index)}
-              key={index}
-            />
-          ))}
-        </BtnWrapper>
-      </AddTaskWrapper>
-    </ModalContainer>
+        <ModalTitle color={ProjectColors}>Ajoute un devoir à faire</ModalTitle>
+        <CrossBtn closeWindow={() => closeWindow} />
+      </TitleWrapper>
+      <BtnWrapper>
+        {subjectTheme.map((sub, index) => (
+          <MatiereBtn
+            subject={sub}
+            handleChange={() => handleChange(index)}
+            key={index}
+          />
+        ))}
+      </BtnWrapper>
+      <TextInput />
+      <ValidateBtn />
+    </AddTaskWrapper>
   )
 }
 
