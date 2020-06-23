5c7e3c9389be315557cd073abebd76e0ef08253e
magb7 <margaux.bedu@gmail.com>
2020-06-23 10:34:14 +0200

    add primary color for the project and border color for each subjects

--- src/theme/Colors.tsx
+++ src/theme/Colors.tsx
@@ -1,26 +1,54 @@
-const ColorsSubjects = [
-  { label: 'Français', color: 'rgba(141, 71, 214, 0.7)', selected: false },
+export const SubjectsColors = [
+  {
+    label: 'Français',
+    colors: {
+      background: 'rgba(141, 71, 214, 0.7)',
+      border: 'rgba(89, 46, 150, 1)'
+    },
+    selected: false
+  },
   {
     label: 'Mathématiques',
-    color: 'rgba(88, 199, 234, 0.7)',
+    colors: {
+      background: 'rgba(88, 199, 234, 0.7)',
+      border: 'rgba(17, 108, 227, 1)'
+    },
     selected: false
   },
   {
     label: 'Langues vivantes',
-    color: 'rgba(44, 116, 251, 0.7)',
-    selected: false
-  },
-  { label: 'SVT', color: 'rgba(105, 209, 130, 0.7)', selected: false },
-  {
-    label: 'Physique_chimie',
-    color: 'rgba(242, 162, 51, 0.7)',
+    colors: {
+      background: 'rgba(44, 116, 251, 0.7)',
+      border: 'rgba(20, 58, 204, 1)'
+    },
     selected: false
   },
   {
-    label: 'Histoire_géographie',
-    color: 'rgba(228, 65, 99, 0.7)',
+    label: 'SVT',
+    colors: {
+      background: 'rgba(105, 209, 130, 0.7)',
+      border: 'rgba(58, 126, 111, 1)'
+    },
+    selected: false
+  },
+  {
+    label: 'Physique Chimie',
+    colors: {
+      background: 'rgba(242, 162, 51, 0.7)',
+      border: 'rgba(238, 130, 51, 1)'
+    },
+    selected: false
+  },
+  {
+    label: 'Histoire Géographie',
+    colors: {
+      background: 'rgba(228, 65, 99, 0.7)',
+      border: 'rgba(167, 49, 95, 1)'
+    },
     selected: false
   }
 ]
 
-export default ColorsSubjects
+export const ProjectColors = {
+  primary: 'rgba(110, 75, 196,1)'
+}
