9b9c3f33d264f1a136cb5ecc549dc6ce6a143bde
magb7 <margaux.bedu@gmail.com>
2020-06-23 16:51:13 +0200

    change style and add placeholder

--- src/components/Text/TextInput.tsx
+++ src/components/Text/TextInput.tsx
@@ -2,9 +2,14 @@ import styled from 'rn-css'
 
 const TextInput = styled.TextInput`
   width: 100%;
-  height: 40%;
+  height: 20%;
   font-size: 1em;
-  border: 1px solid black;
+  border: 1.5px solid black;
   border-radius: 3px;
+  padding: 10px;
+  text-align: center;
+  font-size: 1.5em;
+  font-style: italic;
+  outline: none;
 `
 export default TextInput
