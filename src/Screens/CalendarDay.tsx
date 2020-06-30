b07064c2418013d220245a390ee220a871385dc2
sophie <sneuville59@gmail.com>
2020-06-30 16:36:25 +0200

    add button ton add or remove one day. Nedd to resolve a little problem

--- src/Screens/CalendarDay.tsx
+++ src/Screens/CalendarDay.tsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { View, TouchableOpacity } from 'react-native'
 import styled from 'rn-css'
 import moment from 'moment'
 import DayColumn from '../components/DayColumn'
+import RightArrow from '../components/Button/RightArrow'
+import LeftArrow from '../components/Button/LeftArrow'
 
 const SwitchTextStyled = styled.Text`
   width: 100px;
@@ -30,145 +32,41 @@ moment.locale('fr')
 
 const CalendarDay = () => {
   const today = new Date()
-  // const [number, setNumber] = useState(0)
+  const oneDay = moment(today).format('dddd Do MMMM ')
+  const [day, setDay] = useState(moment(today).format('dddd Do MMMM'))
+  const [numberToAdd, setNumberToAdd] = useState(0)
+  const [numberToRemove, setNumberToRemove] = useState(0)
 
-  const Day = moment(today).format('dddd Do MMMM YYYY')
-  // const todayCalendar = moment(today).format('dddd')
+  const addOneDay = () => {
+    setNumberToAdd(numberToAdd + 1)
+    setDay(
+      moment()
+        .add(numberToAdd - numberToRemove, 'days')
+        .format('dddd Do MMMM')
+    )
+  }
 
-  // const printWeek = (todayCalendar: any) => {
-  //   let toPrint: any = []
-
-  //   switch (todayCalendar) {
-  //     case 'lundi':
-  //       toPrint.push(
-  //         moment()
-  //           .add(number, 'days')
-  //           .format('dddd Do MMMM')
-  //       )
-  //       for (let i = 1; i < 5; i++) {
-  //         toPrint.push(
-  //           moment()
-  //             .add(i + number, 'days')
-  //             .format('dddd Do MMMM')
-  //         )
-  //       }
-
-  //       break
-  //     case 'mardi':
-  //       toPrint = [
-  //         moment()
-  //           .subtract(1 - number, 'days')
-  //           .format('dddd Do MMMM'),
-  //         moment()
-  //           .add(number, 'days')
-  //           .format('dddd Do MMMM')
-  //       ]
-  //       for (let i = 1; i < 4; i++) {
-  //         toPrint.push(
-  //           moment()
-  //             .add(i + number, 'days')
-  //             .format('dddd Do MMMM')
-  //         )
-  //       }
-  //       break
-  //     case 'mercredi':
-  //       toPrint = [
-  //         moment()
-  //           .subtract(2 - number, 'days')
-  //           .format('dddd Do MMMM'),
-  //         moment()
-  //           .subtract(1 - number, 'days')
-  //           .format('dddd Do MMMM'),
-  //         moment()
-  //           .add(number, 'days')
-  //           .format('dddd Do MMMM')
-  //       ]
-  //       for (let i = 1; i < 3; i++) {
-  //         toPrint.push(
-  //           moment()
-  //             .add(i + number, 'days')
-  //             .format('dddd Do MMMM')
-  //         )
-  //       }
-  //       break
-  //     case 'jeudi':
-  //       toPrint = []
-  //       for (let i = 3; i > 0; i--) {
-  //         toPrint.push(
-  //           moment()
-  //             .subtract(i - number, 'days')
-  //             .format('dddd Do MMMM')
-  //         )
-  //       }
-  //       toPrint.push(
-  //         moment()
-  //           .add(number, 'days')
-  //           .format('dddd Do MMMM'),
-  //         moment()
-  //           .add(1 + number, 'days')
-  //           .format('dddd Do MMMM ')
-  //       )
-  //       break
-  //     case 'vendredi':
-  //       toPrint = []
-  //       for (let i = 4; i > 0; i--) {
-  //         toPrint.push(
-  //           moment()
-  //             .subtract(i - number, 'days')
-  //             .format('dddd Do MMMM')
-  //         )
-  //       }
-  //       toPrint.push(
-  //         moment()
-  //           .add(number, 'days')
-  //           .format('dddd Do MMMM')
-  //       )
-  //       break
-  //     case 'samedi':
-  //       toPrint = []
-  //       for (let i = 2; i > 7; i++) {
-  //         toPrint.push(
-  //           moment()
-  //             .add(i + number, 'days')
-  //             .format('dddd Do MMMM')
-  //         )
-  //       }
-  //       break
-  //     case 'dimanche':
-  //       toPrint = []
-  //       for (let i = 1; i > 6; i++) {
-  //         toPrint.push(
-  //           moment()
-  //             .add(i + number, 'days')
-  //             .format('dddd Do MMMM')
-  //         )
-  //       }
-  //   }
-
-  //   return toPrint
-  // }
-  // const returnCurrentWeek = () => {
-  //   setNumber(0)
-  // }
-
-  // const addOneWeek = () => {
-  //   setNumber(number + 7)
-  // }
-
-  // const removeOneWeek = () => {
-  //   setNumber(number - 7)
-  // }
-
+  const removeOneDay = () => {
+    setNumberToRemove(numberToRemove + 1)
+    setDay(
+      moment()
+        .subtract(numberToRemove - numberToAdd, 'days')
+        .format('dddd Do MMMM')
+    )
+  }
   return (
     <>
       <View>
-        <TextCalendar>{Day}</TextCalendar>
+        <TextCalendar>{oneDay}</TextCalendar>
       </View>
-
+      <TouchableOpacity onPress={removeOneDay}>
+        <LeftArrow />
+      </TouchableOpacity>
+      <TouchableOpacity onPress={addOneDay}>
+        <RightArrow />
+      </TouchableOpacity>
       <CalendarView>
-
-        <DayColumn day={Day} />
-
+        <DayColumn day={day} />
       </CalendarView>
 
       <SwitchDayWeekButton />
